6909d3ff9c6fe2c3208c82de63a86e13da5c7889
Thorben Killert <killert@weitkamper.de>
2019-12-09 16:01:19 +0100

    kleine Änderungen und Korrekturen #SANDBOX-1 In Bearbeitung

--- lib/MyModule.php
+++ lib/MyModule.php
@@ -3,12 +3,11 @@ class MyModule {
     /**
      * This ist just a test Module.
      **/
-    private $__name;
     protected $_counter;
     public $id;
     
     public function getCounter(){
-        return $_counter;
+        return $this->_counter;
     }
 }
 
--- lib/bla.php
+++ lib/bla.php
@@ -2,11 +2,3 @@
 
 # comment
 echo "There is no cow level!";
-
-exit;
-<<<<<<< HEAD
-=======
-
-
-doNothing();
->>>>>>> 13fbacfafe55f0fd4567d2afd55d44f495a4fc5b
